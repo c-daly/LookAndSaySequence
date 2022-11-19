7e321cf1ac7c1007807dca3c1629a5cb9d2122fd
c-daly <chris@cdaly.me>
2022-11-19 14:06:20 -0500

    Just started, classes stubbed out

---
+++ src/lookandsee/LookAndSayIterator.java
@@ -0,0 +1,48 @@
+package lookandsee;
+
+import java.math.BigInteger;
+
+public class LookAndSayIterator implements RIterator<BigInteger> {
+    private BigInteger seed;
+    private BigInteger defaultSeed = BigInteger.ZERO;
+    private BigInteger end;
+    private BigInteger current;
+    @Override
+    public BigInteger prev() {
+        return current;
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        if(current != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public BigInteger next() {
+        return current;
+    }
+
+    public LookAndSayIterator(BigInteger seed, BigInteger end) {
+        this.seed = seed;
+        this.end = end;
+    }
+    public LookAndSayIterator(BigInteger seed) {
+        current = seed;
+    }
+
+    public LookAndSayIterator() {
+        this.seed = this.defaultSeed;
+        this.current = this.defaultSeed;
+        this.end = this.defaultSeed;
+
+    }
+}
---
+++ src/lookandsee/RIterator.java
@@ -0,0 +1,8 @@
+package lookandsee;
+
+import java.util.Iterator;
+
+public interface RIterator<T> extends Iterator<T> {
+    T prev();
+    boolean hasPrevious();
+}
